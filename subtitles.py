58e0bfe8a3a9e5178c4ebb3377d407d3f16ed282
DESKTOP-0P21BI7\Tony B <tonybanh@live.com>
2018-09-20 03:54:18 -0700

    Implement selenium to navigate https://www.yify-subtitles.com/, find highest rating for a language, and go to download page

--- subtitles.py
+++ subtitles.py
@@ -5,7 +5,9 @@
 
 from selenium import webdriver
 
-movie = "Deadpool"
+movie = "Deadpool 2"
+languageFilter = "English"
+
 browser = webdriver.Chrome()
 browser.get('https://www.yify-subtitles.com/')
 searchBar = browser.find_element_by_id('qSearch')   # Select the search bar
@@ -26,7 +28,7 @@ elif len(movieLinks) == 1:
 
 else:
     # Multiple results for movie searched. List out each result for user to select from
-    print(f'\Results found for "{movie}" keyword:')
+    print(f'Results found for "{movie}" keyword:')
     for i in range(len(movieLinks)):
         print(f'\t{str(i + 1) + ".":<3}', movieLinks[i].text)
     print()
@@ -47,7 +49,48 @@ else:
     # Click on the selected movie link
     movieLinks[movieChosen - 1].click()
 
-# TODO: Click on the English subtitle link with the highest rating
+# Select all available subtitles
+allSubs = {}
+availableLanguages = [] # Used to validate language filter
+allSubsElem = browser.find_elements_by_css_selector('.other-subs tbody tr')
+
+# Loop through each subtitle and obtain: subtitle id, language, rating, and download link element
+for subElem in allSubsElem:
+    subId = subElem.get_attribute('data-id')   # data-id of each subtitle
+    lang = subElem.find_element_by_css_selector('.sub-lang').text   # Language of each subtitle
+    rating = int(subElem.find_element_by_css_selector('.rating-cell').text) # rating of subtitle
+    downloadLink = subElem.find_element_by_css_selector('a').get_attribute('href')  # download link element
+
+    # Stores all subtitles in a dictionary
+    allSubs[subId] = {
+        'language': lang,
+        'rating': rating,
+        'link': downloadLink
+        }
+
+    # Appends language to availableLanguages list if not yet present
+    if lang not in availableLanguages:
+        availableLanguages.append(lang)
+
+# Validate language filter
+if languageFilter not in availableLanguages:
+    print(f'\n{languageFilter} subtitles not available. Try again...\n')
+    browser.quit()
+    exit()
+
+# Determine the highest rated subtitle
+highestRated = None
+for subId in allSubs.keys():
+    if allSubs[subId]['language'] == languageFilter:
+        if highestRated == None:
+            highestRated = subId
+        else:
+            if allSubs[subId]['rating'] > allSubs[highestRated]['rating']:
+                highestRated = subId
+
+# Open the highest rated link
+browser.get(allSubs[highestRated]['link'])
+
 # TODO: Download the subtitle file
 # TODO: Implement the ability to search for movie subtitles via command prompt
 # TODO: Unzip the file (different python script)
