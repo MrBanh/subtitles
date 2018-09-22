0b87cdadd02429d41ebfb8686459587780f023b7
DESKTOP-0P21BI7\Tony B <tonybanh@live.com>
2018-09-21 23:36:19 -0700

    refactor: language filtering
    Loops through all the subtitles and only add subId, lang, rating, and downloadLink
    to dictionary if the lang is the specified language. The dictionary would only contain
    information on the subtitle links that matches the specified language, less to loop through.
    Previously, it would loop through all subtitles and store all subId, lang, rating,
    and downloadLink and would need to loop through the dictionary with all subtitles
    and identify only those that matches the specified language.

--- subtitles.py
+++ subtitles.py
@@ -9,10 +9,7 @@ movie = "Deadpool 2"
 languageFilter = "English"
 
 browser = webdriver.Chrome()
-browser.get('https://www.yify-subtitles.com/')
-searchBar = browser.find_element_by_id('qSearch')   # Select the search bar
-searchBar.send_keys(movie)  # type to the search bar
-searchBar.submit()  # start the search
+browser.get(f'https://www.yify-subtitles.com/search?q={movie}')
 
 # Gather all search results that match the movie that the user searched for
 movieLinks = browser.find_elements_by_css_selector('a div h3[itemprop="name"]')
@@ -50,46 +47,42 @@ else:
     movieLinks[movieChosen - 1].click()
 
 # Select all available subtitles
-allSubs = {}
-availableLanguages = [] # Used to validate language filter
+filteredSubs = {}
 allSubsElem = browser.find_elements_by_css_selector('.other-subs tbody tr')
 
-# Loop through each subtitle and obtain: subtitle id, language, rating, and download link element
+# Loop through each subtitle and determine if the language matches the specified language
+# If so, obtain the subtitle id, rating, and download link element
 for subElem in allSubsElem:
-    subId = subElem.get_attribute('data-id')   # data-id of each subtitle
     lang = subElem.find_element_by_css_selector('.sub-lang').text   # Language of each subtitle
-    rating = int(subElem.find_element_by_css_selector('.rating-cell').text) # rating of subtitle
-    downloadLink = subElem.find_element_by_css_selector('a').get_attribute('href')  # download link element
+    if lang == languageFilter:
+        subId = subElem.get_attribute('data-id')   # data-id of each subtitle
+        rating = int(subElem.find_element_by_css_selector('.rating-cell').text) # rating of subtitle
+        downloadLink = subElem.find_element_by_css_selector('a').get_attribute('href')  # download link element
 
-    # Stores all subtitles in a dictionary
-    allSubs[subId] = {
-        'language': lang,
-        'rating': rating,
-        'link': downloadLink
-        }
+        # Stores subtitles in a dictionary
+        filteredSubs[subId] = {
+            'language': lang,
+            'rating': rating,
+            'link': downloadLink
+            }
 
-    # Appends language to availableLanguages list if not yet present
-    if lang not in availableLanguages:
-        availableLanguages.append(lang)
-
-# Validate language filter
-if languageFilter not in availableLanguages:
+# Give user an error if subtitles for the language is not available
+if not filteredSubs:
     print(f'\n{languageFilter} subtitles not available. Try again...\n')
     browser.quit()
     exit()
 
 # Determine the highest rated subtitle
 highestRated = None
-for subId in allSubs.keys():
-    if allSubs[subId]['language'] == languageFilter:
-        if highestRated == None:
+for subId in filteredSubs.keys():
+    if highestRated == None:
+        highestRated = subId
+    else:
+        if filteredSubs[subId]['rating'] > filteredSubs[highestRated]['rating']:
             highestRated = subId
-        else:
-            if allSubs[subId]['rating'] > allSubs[highestRated]['rating']:
-                highestRated = subId
 
 # Open the highest rated link
-browser.get(allSubs[highestRated]['link'])
+browser.get(filteredSubs[highestRated]['link'])
 
 # TODO: Download the subtitle file
 # TODO: Implement the ability to search for movie subtitles via command prompt
