b291c9ac267bdffb1987a1e9261363e8badb9a44
DESKTOP-0P21BI7\Tony B <tonybanh@live.com>
2019-01-21 06:18:32 -0800

    style: format to be more pythonic

--- findSubtitles.py
+++ findSubtitles.py
@@ -1,12 +1,15 @@
-# findSubtitles.py - Search https://www.yify-subtitles.com/ and download the highest rated subtitle
+# findSubtitles.py - Search https://www.yify-subtitles.com/ and download the
+# highest rated subtitle
 
 from selenium import webdriver
 import os
 from unzip import unzip
 
-def findSubtitles(movie, languageFilter='English'):
 
-    # Obtain user's desktop directory and set it as the directory where the file will be downloaded to
+def find_subs(movie, language_filter='English'):
+
+    # Obtain user's desktop directory and set it as the directory where the
+    # file will be downloaded to
     desktop = os.path.join(os.environ['USERPROFILE'], 'Desktop\\')
     options = webdriver.ChromeOptions()
     prefs = {'download.default_directory': desktop}
@@ -16,37 +19,41 @@ def findSubtitles(movie, languageFilter='English'):
     browser = webdriver.Chrome(chrome_options=options)
     browser.get(f'https://www.yify-subtitles.com/search?q={movie}')
 
-    # Gather all search results that match the movie that the user searched for
-    movieLinks = browser.find_elements_by_css_selector('.media-list .media.media-movie-clickable')
-    # for link in clickable_movie:
-    #     print(link.find_element_by_css_selector('h3[itemprop="name"]').text)
-    # movieLinks = browser.find_elements_by_css_selector('a div h3[itemprop="name"]')
+    # Gather all search results that match the movie searched for
+    search_results = browser.find_elements_by_css_selector('.media-list \
+                                                .media.media-movie-clickable')
 
-    if len(movieLinks) == 0:
+    if len(search_results) == 0:
         # Did not find subtitles for movie
         print(f'No results for "{movie}"')
         browser.quit()  # close the browser
 
-    elif len(movieLinks) == 1:
+    elif len(search_results) == 1:
         # Only 1 result for movie searched. Open the link
-        movieLinks[0].click()
+        search_results[0].click()
 
     else:
-        # Multiple results for movie searched. List out each result for user to select from
+        # Multiple results for movie searched. List out each result for user
+        # to select from
         print(f'Results found for "{movie}" keyword:')
-        for i in range(len(movieLinks)):
-            movie_name = movieLinks[i].find_element_by_css_selector('h3[itemprop="name"]').text
-            movie_year = movieLinks[i].find_element_by_css_selector('.movinfo-section').text[0:4]
+        for i in range(len(search_results)):
+            movie_name = search_results[i].find_element_by_css_selector(
+                    'h3[itemprop="name"]').text
+            movie_year = search_results[i].find_element_by_css_selector(
+                    '.movinfo-section').text[0:4]
             print(f'\t{str(i + 1) + ".":<3} {movie_name} ({movie_year})')
         print()
 
         while True:
             try:
                 # Have user choose the movie from search results
-                movieChosen = int(input(f'Please choose a movie (1 - {len(movieLinks)}): '))
+                movie_chosen = int(input(f'Please choose a movie'
+                                         f' (1 - {len(search_results)}): '))
 
-                # Validate user input: must be integer, and within range of movies listed
-                if movieChosen < 1 or movieChosen > len(movieLinks) or isinstance(movieChosen, float):
+                # Validate user input: must be integer, and within range of
+                # movies listed
+                if movie_chosen < 1 or movie_chosen > len(search_results) \
+                        or isinstance(movie_chosen, float):
                     raise Exception
 
                 break   # Exits loop
@@ -54,51 +61,67 @@ def findSubtitles(movie, languageFilter='English'):
                 print('Invalid input.', end=' ')
 
         # Click on the selected movie link
-        movieLinks[movieChosen - 1].click()
+        search_results[movie_chosen - 1].click()
 
     # Select all available subtitles
-    filteredSubs = {}
-    allSubsElem = browser.find_elements_by_css_selector('.other-subs tbody tr')
+    filtered_subs = {}
+    all_subs_elem = browser.find_elements_by_css_selector(
+            '.other-subs tbody tr')
 
-    # Loop through each subtitle and determine if the language matches the specified language
-    # If so, obtain the subtitle id, rating, and download link element
-    for subElem in allSubsElem:
-        lang = subElem.find_element_by_css_selector('.sub-lang').text   # Language of each subtitle
-        if lang == languageFilter:
-            subId = subElem.get_attribute('data-id')   # data-id of each subtitle
-            rating = int(subElem.find_element_by_css_selector('.rating-cell').text) # rating of subtitle
-            toDownloadPage = subElem.find_element_by_css_selector('a').get_attribute('href')  # subtitle's download page link
+    # Loop through each subtitle and determine if the language matches the
+    # specified language. If so, obtain the subtitle id,
+    # rating, and download link element
+    for subElem in all_subs_elem:
+        # Language of each subtitle
+        lang = subElem.find_element_by_css_selector('.sub-lang').text
+        if lang == language_filter:
+
+            # data-id of each subtitle
+            sub_id = subElem.get_attribute('data-id')
+
+            # rating of subtitle
+            rating = int(subElem.find_element_by_css_selector(
+                    '.rating-cell').text)
+
+            # subtitle's download page link
+            to_dl_page = subElem.find_element_by_css_selector(
+                    'a').get_attribute('href')
 
             # Stores subtitles in a dictionary
-            filteredSubs[subId] = {
+            filtered_subs[sub_id] = {
                 'language': lang,
                 'rating': rating,
-                'downloadPage': toDownloadPage
+                'downloadPage': to_dl_page
                 }
 
     # Give user an error if subtitles for the language is not available
-    if not filteredSubs:
-        print(f'\n{languageFilter} subtitles not available. Try again...\n')
+    if not filtered_subs:
+        print(f'\n{language_filter} subtitles not available. Try again...\n')
         browser.quit()
         exit()
 
     # Determine the highest rated subtitle
-    highestRated = None
-    for subId in filteredSubs.keys():
-        if highestRated == None:
-            highestRated = subId
+    highest_rated = None
+    for sub_id in filtered_subs.keys():
+        if highest_rated is None:
+            highest_rated = sub_id
         else:
-            if filteredSubs[subId]['rating'] > filteredSubs[highestRated]['rating']:
-                highestRated = subId
+            if filtered_subs[sub_id]['rating'] > \
+                    filtered_subs[highest_rated]['rating']:
+                highest_rated = sub_id
 
     # Open the highest rated link
-    browser.get(filteredSubs[highestRated]['downloadPage'])
+    browser.get(filtered_subs[highest_rated]['downloadPage'])
 
     # Download the subtitle file
-    clickToDownload = browser.find_element_by_partial_link_text('DOWNLOAD SUBTITLE')    # select the <a> element to download subtitle
-    filename = os.path.basename(clickToDownload.get_attribute('href'))  # Obtain name of zip file based on download link
+    click_to_download = browser.find_element_by_partial_link_text(
+            'DOWNLOAD SUBTITLE')    # select the <a> element to download
+
+    # Obtain name of zip file based on download link
+    filename = os.path.basename(click_to_download.get_attribute('href'))
+
     print('Downloading subtitles...')
-    clickToDownload.click()
+    click_to_download.click()
 
     # Unzip the file
     import time
--- subtitles.py
+++ subtitles.py
@@ -1,17 +1,19 @@
 #! python3
 
-# subtitles.py - Search https://www.yify-subtitles.com/ 
-#               Downloads the highest rated subtitle (based on language specified, default is English)
-#               Unzips the downloaded zipped file and send the zipped file to trash once extracted.
+# subtitles.py - Search https://www.yify-subtitles.com/
+#               Downloads the highest rated subtitle (based on language
+#               specified, default is English)
+#               Unzips the downloaded zipped file and
+#               send the zipped file to trash once extracted.
 
 import sys
-from findSubtitles import findSubtitles
+from findSubtitles import find_subs
 
 # Implement the ability to search for movie subtitles via command prompt
 if len(sys.argv) == 1:
-    print('\nType in command prompt to find subtitles:\n') 
+    print('\nType in command prompt to find subtitles:\n')
     print('\t' + '\033[43m' + 'subtitles <movie>' + '\033[0m' + '\n')
 
 elif len(sys.argv) > 1:
     movieName = ' '.join(sys.argv[1:])
-    findSubtitles(movieName)
+    find_subs(movieName)
